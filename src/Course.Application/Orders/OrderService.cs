56dd7598a68f5277f888448df22eadfa53ac1ecb
Yael Chavez <you400@gmail.com>
2026-07-28 18:34:25 -0600

    some chn

--- src/Course.Application/Orders/OrderService.cs
+++ src/Course.Application/Orders/OrderService.cs
@@ -51,7 +51,7 @@ public sealed class OrderService : IOrderService
         }
 
         var payment = await _payments.PayAsync(order.Id, order.CalculateTotal(), cancellationToken);
-        order.RegisterPayment(payment);
+//        order.RegisterPayment(payment);
 
 
       //  await _orders.AddAsync(order, cancellationToken);
